fd65fb06838fdcf80ed09af8985ddcdd24248280
3987q <39289871+3987q@users.noreply.github.com>
2018-05-17 04:13:10 +0300

    Update xmlFunctions.cs

--- xmlFunctions.cs
+++ xmlFunctions.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows.Forms;
+using System.Windows.Forms;
 using System.Xml;
 using System;
 using System.Configuration;
@@ -17,29 +17,41 @@ namespace Rusal_test_task
                     string[] keys = { "Потребление Ввод #1", "Потребление Ввод #2" };
                     XmlDocument xDoc = new XmlDocument();
                     xDoc.Load(OPF.OpenFile());
+                    // Если целостность тегов будет нарушена, то выдаст необработанное исключение. Правится try/catch для каждего исользуемого экземпляра XmlElement.
                     XmlElement xRoot = xDoc.DocumentElement;
-                    foreach (XmlNode xnode in xRoot)
-                        if (xnode.Name == "area")
-                            foreach (XmlNode xChildNode in xnode.ChildNodes)
-                                //Предположил, что в конфиге должен указывать порядковый номер "Основной фидер №...", а затем собираю все вложенные <value>
-                                if ((xChildNode.Name == "measuringpoint") && ((xChildNode.Attributes.GetNamedItem("name").Value.Contains(ConfigurationManager.AppSettings[keys[0]]) == true) ||
-                                      (xChildNode.Attributes.GetNamedItem("name").Value.Contains(ConfigurationManager.AppSettings[keys[1]]) == true)))
-                                    foreach (XmlNode xMeasuringchannel in xChildNode)
-                                        foreach (XmlNode xPeriod in xMeasuringchannel)
-                                        {
-                                            try
-                                            {
+                    if (isXmlCorrect(xRoot))
+                    {
+                        button2.Enabled = true;
+                        errorProvider1.SetError(button1, "");
+                        foreach (XmlNode xnode in xRoot)
+                            if (xnode.Name == "area")
+                                foreach (XmlNode xChildNode in xnode.ChildNodes)
+                                    //Предположил, что в конфиге должен указывать порядковый номер "Основной фидер №...", а затем собираю все вложенные <value>
+                                    if ((xChildNode.Name == "measuringpoint") && ((xChildNode.Attributes.GetNamedItem("name").Value.Contains(ConfigurationManager.AppSettings[keys[0]]) == true) ||
+                                          (xChildNode.Attributes.GetNamedItem("name").Value.Contains(ConfigurationManager.AppSettings[keys[1]]) == true)))
+                                        foreach (XmlNode xMeasuringchannel in xChildNode)
+                                            foreach (XmlNode xPeriod in xMeasuringchannel)
                                                 consumptionCurrent += Convert.ToDouble(xPeriod.FirstChild.InnerText.ToString());
-                                                button2.Enabled = true;
-                                                errorProvider1.SetError(button1, "");
-                                            }
-                                            catch
-                                            {
-                                                //Если файл с иной разметкой, то произвести рассчет нельзя
-                                                button2.Enabled = false;
-                                                errorProvider1.SetError(button1, "Некорректный .xml файл");
-                                            }
-                                        }
+                    }
+                    else
+                    {
+                        button2.Enabled = false;
+                        errorProvider1.SetError(button1, "Некорректный .xml файл");
+                    }
+            }
+        }
+        // пример валидации разметки по номеру класса рутовго тега
+        bool isXmlCorrect(XmlElement root)
+        {   
+            try
+            { 
+                if (root.Attributes.GetNamedItem("class").Value.Contains("88020"))
+                    return true;
+                else return false;
+            }
+            catch
+            {
+                return false;
             }
         }
     }
